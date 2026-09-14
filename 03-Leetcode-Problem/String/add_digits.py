e7cf3bc66c208624ec5a29636231f4dfc4e87c82
Mamun Bepari <beparimamun708@gmail.com>
2026-09-14 15:32:32 +0600

    add_digits problem is completed

---
+++ 03-Leetcode-Problem/String/add_digits.py
@@ -0,0 +1,18 @@
+
+
+class Solution:
+   def addDigits(self, num: int) -> int:
+
+      while num >= 10:
+
+         digit_sum = 0
+
+         while num > 0:
+            last_digit = num % 10
+            digit_sum += last_digit
+            num //= 10
+
+         num = digit_sum
+
+      return num
+
