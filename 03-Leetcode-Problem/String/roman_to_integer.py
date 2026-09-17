2e6391808dece4fe12b2fd33da0de4233d35983f
Mamun Bepari <beparimamun708@gmail.com>
2026-09-17 18:42:50 +0600

    Completed romanToInt problem

---
+++ 03-Leetcode-Problem/String/roman_to_integer.py
@@ -0,0 +1,35 @@
+
+
+class Solution:
+   def romanToInt(self, s: str) ->int:
+      values = {
+         "I": 1,
+         "V": 5,
+         "X": 10,
+         "L": 50,
+         "C": 100,
+         "D": 500,
+         "M": 1000,
+      }
+
+      total = 0
+      for i in range(len(s)):
+         current = values[s[i]]
+
+         if i + 1 < len(s):
+            next_value = values[s[i+1]]
+
+            if current < next_value:
+               total -= current
+            else:
+               total += current
+
+         else:
+            total += current
+
+      return total 
+
+  
+
+         
+      
