3b6d50b7f61e09702abd4d6492ae14a1587ea4f5
Mamun Bepari <beparimamun708@gmail.com>
2026-09-18 10:01:54 +0600

    Add interview answer

--- 03-Leetcode-Problem/String/reverse_string.py
+++ 03-Leetcode-Problem/String/reverse_string.py
@@ -10,4 +10,12 @@ class Solution:
          left += 1
          right -= 1
 
-         
+
+"""
+
+I use two pointers, one at the biginning and one at the end of the array.
+I swap the characters at these two positions and then move the left pointer forward and right pointer backward.
+I continue until the pointers meet.
+This reverses the array in-place with 0(n) time and 0(1) extra space.
+
+"""
