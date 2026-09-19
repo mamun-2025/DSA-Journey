fcad4c37b821685d0047aef621821460cc230c50
Mamun Bepari <beparimamun708@gmail.com>
2026-09-20 00:59:36 +0600

    Completed lenghtOfLastWord problem

--- 03-Leetcode-Problem/String/length_of_last_word.py
+++ 03-Leetcode-Problem/String/length_of_last_word.py
@@ -1,3 +1,35 @@
 
 
+class Solution:
+   def lengthOfLastWord(self, s: str) ->int:
 
+      right = len(s) - 1
+      count = 0
+
+      while right >= 0 and s[right] == " ":
+         right -= 1
+
+      while right >= 0 and s[right] != " ":
+         count += 1
+         right -= 1
+
+      return count
+
+
+"""
+
+I start form the end of the string and skip all trailing spaces.
+Then I count characters until I reach a space.
+The count is the length of the last word.
+
+I traverse the string from right to left, skip trailing spaces, and count the characters of the last word.
+
+traverse from right to left → ডান থেকে বামে যাওয়া
+trailing spaces → শেষে থাকা spaces
+count characters → character গণনা করা
+last word → শেষ শব্দ
+
+Time Complexity = 0(n)
+Space Complexity = 0(1)
+
+"""
