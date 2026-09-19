522fab5e146b095f1fafe5e879b23b2fef87aaae
Mamun Bepari <beparimamun708@gmail.com>
2026-09-20 02:32:27 +0600

    Complete basic logic positive_negative_zero

--- 01-Basic-Logic-Problem/even-or-odd.py
+++ 01-Basic-Logic-Problem/even-or-odd.py
@@ -6,3 +6,10 @@ if n % 2 == 0:
    print(f"{n}- is a Even number.")
 else: 
    print(f"{n}- is a Odd number.")
+
+"""
+I use the modulo operator.
+If the remainder after dividing the number by 2 is zero, the number is even; otherwise, it is odd.
+The time complexity is 0(1) and the space complexity is 0(1).
+
+"""
---
+++ 01-Basic-Logic-Problem/positive_negative_zero.py
@@ -0,0 +1,20 @@
+
+
+n = int(input("Enter a number: "))
+
+if n > 0:
+   print("Positive")
+elif n < 0:
+   print("Negative")
+else:
+   print("Zero")
+
+
+"""
+I compare the number with zero.
+If the number is greater than zero, it is positive.
+If it is less than zero, it is negative.
+Otherwise, it is zero.
+The time complexity is 0(1) and space complexity is 0(1).
+
+"""
