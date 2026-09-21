c2e371e186f58145f6a9ada7306a9ac0a2dc4937
Mamun Bepari <beparimamun708@gmail.com>
2026-09-22 03:42:36 +0600

    Complete longest common prefix problem

--- 03-Leetcode-Problem/String/longest_common_prefix.py
+++ 03-Leetcode-Problem/String/longest_common_prefix.py
@@ -1,2 +1,28 @@
 
 
+class Solution:
+   def longestCommonPrefix(self, strs: list[str]) ->str:
+      result = ""
+
+      for i in range(len(strs[0])):
+         for word in strs:
+            if i >= len(word) or word[i] != strs[0][i]:
+               return result 
+
+         result += strs[0][i]
+
+      return result
+
+         
+"""
+I use the first string as a reference 
+and compare each character position with the same position in every other string.
+If a string ends or a character doesn't match,
+I immediately return the prefix found so far.
+
+n = number of strings
+m = length of the shortest string
+time = 0(nxm) 
+space = 0(1)
+
+"""
